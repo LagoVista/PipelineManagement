fe53924a47f0202593d782fb4526739e0c9ba8e7
Kevin D. Wolf <kevinw@software-logistics.com>
2017-04-25 14:37:05 -0400

    update nuget spec

---
+++ tests/LagoVista.IoT.Pipeline.Tests/Parsers/RegExValidationTests.cs
@@ -0,0 +1,63 @@
+﻿using LagoVista.Core.Models;
+using LagoVista.Core.PlatformSupport;
+using LagoVista.IoT.Pipeline.Admin.Models;
+using LagoVista.IoT.Pipeline.Standard.Parsers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LagoVista.IoT.Pipeline.Tests.Parsers
+{
+    [TestClass]
+    public class RegExValidationTests
+    {
+        Moq.Mock<ILogger> _logger;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _logger = new Moq.Mock<ILogger>();
+        }
+
+
+        /// <summary>
+        /// Note this is testing the abstract base class method to use validation to look at the result
+        /// </summary>
+        [TestMethod]
+        public void ValidatesWithRegExValidation()
+        {
+            var parser = new DelimitedColumnParser(new MessageFieldParserConfiguration()
+            {
+                ParserStrategy = new EntityHeader<ParserStrategies>() { Value = ParserStrategies.DelimitedColumn },
+                DelimitedColumnIndex = 2,
+                Delimiter = ",",
+                QuotedText = true,
+                RegExValidation = "^\\w{6}$"
+            }, _logger.Object);
+
+            var result = parser.Parse(new Runtime.Core.Models.PEM.PipelineExectionMessage() { TextPayload = "432434,\"ABC123\",52134,27", PayloadType = Runtime.Core.Models.PEM.MessagePayloadTypes.Text });
+            Assert.IsTrue(result.Success);
+            Assert.AreEqual("ABC123", result.Result);
+        }
+
+        /// <summary>
+        /// Note this is testing the abstract base class method to use validation to look at the result
+        /// </summary>
+        [TestMethod]
+        public void DoesNotValidatesWithRegExValidation()
+        {
+            var parser = new DelimitedColumnParser(new MessageFieldParserConfiguration()
+            {
+                ParserStrategy = new EntityHeader<ParserStrategies>() { Value = ParserStrategies.DelimitedColumn },
+                DelimitedColumnIndex = 2,
+                Delimiter = ",",
+                QuotedText = true,
+                RegExValidation = "^\\w{7}$"
+            }, _logger.Object);
+
+            var result = parser.Parse(new Runtime.Core.Models.PEM.PipelineExectionMessage() { TextPayload = "432434,\"ABC123\",52134,27", PayloadType = Runtime.Core.Models.PEM.MessagePayloadTypes.Text });
+            Assert.IsFalse(result.Success);
+        }
+    }
+}
---
+++ tests/LagoVista.IoT.Pipeline.Tests/Parsers/ScriptParseTests.cs
@@ -0,0 +1,40 @@
+﻿using LagoVista.Core.Models;
+using LagoVista.Core.PlatformSupport;
+using LagoVista.IoT.Pipeline.Admin.Models;
+using LagoVista.IoT.Pipeline.Standard.Parsers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LagoVista.IoT.Pipeline.Tests.Parsers
+{
+    [TestClass]
+    public class ScriptParseTests
+    {
+        Moq.Mock<ILogger> _logger;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _logger = new Moq.Mock<ILogger>();
+        }
+
+        [TestMethod]
+        public void TestDeviceIdHit()
+        {
+            var parser = new ScriptParser(new MessageFieldParserConfiguration()
+            {
+                ParserStrategy = new EntityHeader<ParserStrategies>() { Id = MessageFieldParserConfiguration.ParserTypeScript, Value = ParserStrategies.Script},
+                RegExLocator = "^\\$(?'DeviceId'[\\w]{3,10}),",
+                RegExGroupName = "DeviceId"
+            }, _logger.Object);
+
+            var result = parser.Parse(new Runtime.Core.Models.PEM.PipelineExectionMessage() { TextPayload = "$ABC123,XXX,YYY,ZZZ!" });
+       //     Assert.IsTrue(result.Success);
+         //   Assert.AreEqual("ABC123", result.Result);
+        }
+
+
+    }
+}
