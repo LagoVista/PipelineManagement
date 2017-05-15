a88b10c56b5985e866412f25a8a058581b392403
Kevin D. Wolf <kevinw@software-logistics.com>
2017-05-15 15:50:45 -0400

    NUGET Version 0.8.165-alpha1505

---
+++ tests/LagoVista.IoT.PipelineAdmin.tests/ParserConfigTests/DelimitedColumnTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LagoVista.IoT.PipelineAdmin.tests.ParserConfigTests
+{
+    [TestClass]
+    public class DelimitedColumnTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ tests/LagoVista.IoT.PipelineAdmin.tests/ParserConfigTests/XmlPropertyTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LagoVista.IoT.PipelineAdmin.tests.ParserConfigTests
+{
+    [TestClass]
+    public class XmlPropertyTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
