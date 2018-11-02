fc7161596d468b79544fe101ce60da7e6e146171
Kevin D. Wolf <kevinw@software-logistics.com>
2018-11-02 17:41:13 -0400

    fix operator bug

--- src/LagoVista.IoT.DataStreamConnectors/PostgresqlConnector.cs
+++ src/LagoVista.IoT.DataStreamConnectors/PostgresqlConnector.cs
@@ -455,7 +455,9 @@ WHERE table_schema = @dbschema
             }
 
             sql.AppendLine($"  order by {_stream.TimeStampFieldName} desc");
-            sql.AppendLine($"   LIMIT {request.PageSize} OFFSET {request.PageSize * request.PageIndex - 1} ");
+            sql.AppendLine($"   LIMIT {request.PageSize} OFFSET {request.PageSize * (request.PageIndex - 1)} ");
+
+            _logger.AddCustomEvent(LogLevel.Message, "GetItemsAsync", sql.ToString());
 
             var responseItems = new List<DataStreamResult>();
 
@@ -484,6 +486,7 @@ WHERE table_schema = @dbschema
                 foreach (var filterItem in filter)
                 {
                     cmd.Parameters.AddWithValue($"@parm{filterItem.Key}", filterItem.Value);
+                    _logger.AddCustomEvent(LogLevel.Message, "GetItemsAsync", $"{filterItem.Key} - {filterItem.Value}");
                 }
 
                 cmd.CommandType = System.Data.CommandType.Text;
